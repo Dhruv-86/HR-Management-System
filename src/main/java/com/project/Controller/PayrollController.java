71ec518832d73a9b71c963c54035a09b3463b15b
Dhruv-86 <dhruvrpatel86@gmail.com>
2024-07-03 18:44:43 -0500

    change in payrole

--- src/main/java/com/project/Controller/PayrollController.java
+++ src/main/java/com/project/Controller/PayrollController.java
@@ -38,12 +38,11 @@ public class PayrollController {
 	@Autowired
 	private DesignationService designationService;
 	@Autowired
-	private AttendenceService attendenceService; 
+	private AttendenceService attendenceService;
 	@Autowired
-	private AllowanceDeductionService allowanceDeductionService ;
+	private AllowanceDeductionService allowanceDeductionService;
 	@Autowired
 	private SalarySlipService salarySlipService;
-	
 
 	@GetMapping(value = "admin/addPayroll")
 	public ModelAndView addPayroll() {
@@ -65,15 +64,17 @@ public class PayrollController {
 		List<PayrollVO> payrollList = this.payrollservice.search();
 		return new ModelAndView("admin/viewPayroll", "payrollList", payrollList);
 	}
+
 	@GetMapping(value = "admin/generatepay")
 	public ModelAndView viewgeneratepay() {
 		List<SalarySlipVO> employeeList = this.salarySlipService.search();
 		List employeeList1 = this.employeeService.search();
 		List attendenceList = this.attendenceService.search();
-		System.out.println("SalaryList::::::"+employeeList);
-		return new ModelAndView("admin/generatepay", "employeeList", employeeList).addObject("attendenceList", attendenceList).addObject("employeeList1", employeeList1);
+		System.out.println("SalaryList::::::" + employeeList);
+		return new ModelAndView("admin/generatepay", "employeeList", employeeList)
+				.addObject("attendenceList", attendenceList).addObject("employeeList1", employeeList1);
 	}
- 
+
 	@GetMapping(value = "admin/deletePayroll")
 	public ModelAndView deletePayroll(@ModelAttribute PayrollVO payrollVO, @RequestParam int id) {
 		List<PayrollVO> payrollList = this.payrollservice.findById(id);
@@ -92,133 +93,135 @@ public class PayrollController {
 				designationSearch);
 	}
 
-	@GetMapping(value =  "admin/CalculatePayment")
-	public ModelAndView CalculatePayroll(@ModelAttribute EmployeeVO employeeVO,@RequestParam int employeeTableId , HttpSession httpSession)
-	{
-		/*System.out.println(employeeTableId);
-		
-		List employeeList = this.employeeService.findById(employeeTableId);
-		List employeeAttendanceList = this.attendenceService.findByEmpId(employeeTableId);
-		List emplBasicSalaryList = this.payrollservice.findByDesignationID(designationId);
-		List emplAllowanceDeductionList = this.allowanceDeductionService.findByDesignationId(designationId);
-		
-		AttendenceVO attendenceVO;
-		
-		Iterator av = employeeAttendanceList.iterator();
-		Double workingDays = null ;
-		Double presentDays = null;
-		while(av.hasNext())
-		{
-			attendenceVO = (AttendenceVO)av.next();
-			workingDays = attendenceVO.getNumberOfWorkingDays();
-			presentDays = attendenceVO.getPresentDays();
-			System.out.println(presentDays);
-		}
-		
-		PayrollVO payrollVO = (PayrollVO) emplBasicSalaryList.get(0);
-		
-		String bs = payrollVO.getBasicSalary();
-		double basicSalary = Double.parseDouble(bs);
-		int allowanceLimit=0;
-		int deductionLimit=0;
-		Iterator i = emplAllowanceDeductionList.iterator();
-		AllowanceDeductionVO allowanceDeductionVO ;
-		int j=0;
-		while(i.hasNext())
-		{
-			//allowanceDeductionVO = (AllowanceDeductionVO) emplAllowanceDeductionList.get(j);
-			allowanceDeductionVO = (AllowanceDeductionVO) i.next();
-			j++;
-			//System.out.println(j);
-			String ad = allowanceDeductionVO.getAllowancdeduction();
-			if(ad.equals("allowance"))
-			{
-				allowanceLimit=allowanceLimit+allowanceDeductionVO.getAllowancelimit();
-				System.out.println("allowance "+allowanceLimit);
-			}
-			else if(ad.equals("deduction"))
-			{
-				deductionLimit = deductionLimit +allowanceDeductionVO.getAllowancelimit();
-				System.out.println("deduction "+deductionLimit);
-			}
-		}
-		double emplfinalPayroll = this.payrollservice.calculatePayroll(workingDays, presentDays, basicSalary , allowanceLimit, deductionLimit );
-		System.out.println(emplfinalPayroll);*/
-		String path = httpSession.getServletContext().getRealPath("/")+"documents//salaryslip";
-		
+	@GetMapping(value = "admin/CalculatePayment")
+	public ModelAndView CalculatePayroll(@ModelAttribute EmployeeVO employeeVO, @RequestParam int employeeTableId,
+			HttpSession httpSession) {
+		/*
+		 * System.out.println(employeeTableId);
+		 * 
+		 * List employeeList = this.employeeService.findById(employeeTableId);
+		 * List employeeAttendanceList =
+		 * this.attendenceService.findByEmpId(employeeTableId);
+		 * List emplBasicSalaryList =
+		 * this.payrollservice.findByDesignationID(designationId);
+		 * List emplAllowanceDeductionList =
+		 * this.allowanceDeductionService.findByDesignationId(designationId);
+		 * 
+		 * AttendenceVO attendenceVO;
+		 * 
+		 * Iterator av = employeeAttendanceList.iterator();
+		 * Double workingDays = null ;
+		 * Double presentDays = null;
+		 * while(av.hasNext())
+		 * {
+		 * attendenceVO = (AttendenceVO)av.next();
+		 * workingDays = attendenceVO.getNumberOfWorkingDays();
+		 * presentDays = attendenceVO.getPresentDays();
+		 * System.out.println(presentDays);
+		 * }
+		 * 
+		 * PayrollVO payrollVO = (PayrollVO) emplBasicSalaryList.get(0);
+		 * 
+		 * String bs = payrollVO.getBasicSalary();
+		 * double basicSalary = Double.parseDouble(bs);
+		 * int allowanceLimit=0;
+		 * int deductionLimit=0;
+		 * Iterator i = emplAllowanceDeductionList.iterator();
+		 * AllowanceDeductionVO allowanceDeductionVO ;
+		 * int j=0;
+		 * while(i.hasNext())
+		 * {
+		 * //allowanceDeductionVO = (AllowanceDeductionVO)
+		 * emplAllowanceDeductionList.get(j);
+		 * allowanceDeductionVO = (AllowanceDeductionVO) i.next();
+		 * j++;
+		 * //System.out.println(j);
+		 * String ad = allowanceDeductionVO.getAllowancdeduction();
+		 * if(ad.equals("allowance"))
+		 * {
+		 * allowanceLimit=allowanceLimit+allowanceDeductionVO.getAllowancelimit();
+		 * System.out.println("allowance "+allowanceLimit);
+		 * }
+		 * else if(ad.equals("deduction"))
+		 * {
+		 * deductionLimit = deductionLimit +allowanceDeductionVO.getAllowancelimit();
+		 * System.out.println("deduction "+deductionLimit);
+		 * }
+		 * }
+		 * double emplfinalPayroll = this.payrollservice.calculatePayroll(workingDays,
+		 * presentDays, basicSalary , allowanceLimit, deductionLimit );
+		 * System.out.println(emplfinalPayroll);
+		 */
+		String path = httpSession.getServletContext().getRealPath("/") + "documents//salaryslip";
+
 		employeeVO.setEmployeeTableId(employeeTableId);
-		
-		
+
 		/*********************** EMPLOYEE LIST ***********************/
 		List employeeList = employeeService.findById(employeeTableId);
-		
-		EmployeeVO employeeVO2 = (EmployeeVO)employeeList.get(0);
-		
+
+		EmployeeVO employeeVO2 = (EmployeeVO) employeeList.get(0);
+
 		/*********************** ATTENDANCE LIST ***********************/
 		List attendanceList = this.attendenceService.findByEmpId(employeeVO2.getEmployeeTableId());
-		
-		AttendenceVO attendanceVO = null; 
+
+		AttendenceVO attendanceVO = null;
 		Iterator av = attendanceList.iterator();
-		
-		while(av.hasNext())
-		{
-			attendanceVO = (AttendenceVO)av.next();
-			
+
+		while (av.hasNext()) {
+			attendanceVO = (AttendenceVO) av.next();
+
 		}
 		attendanceVO.setPaySlipStatus("Generated");
-		System.out.println("paySlipStatus:::::::::"+attendanceVO.getPaySlipStatus());
-		System.out.println("attendanceId:::::::::"+attendanceVO.getId());
+		System.out.println("paySlipStatus:::::::::" + attendanceVO.getPaySlipStatus());
+		System.out.println("attendanceId:::::::::" + attendanceVO.getId());
 		this.attendenceService.save(attendanceVO);
 		int designationId1 = employeeVO2.getDesignationVO().getId();
-		
+
 		/*********************** SALARY LIST ***********************/
 		List salaryList = payrollservice.findByDesignationID(designationId1);
-		
-		
-		PayrollVO salaryVO= (PayrollVO)salaryList.get(0);
-		
+
+		PayrollVO salaryVO = (PayrollVO) salaryList.get(0);
+
 		String basicSalary = salaryVO.getBasicSalary();
-		
+
 		List allowanceVOList = allowanceDeductionService.findByDesignationId(designationId1);
 		List allowanceList = new ArrayList<Object>();
 		List deductionList = new ArrayList<Object>();
 
-		for(int i = 0 ; i  < allowanceVOList.size() ; i++){
-		
-			AllowanceDeductionVO  allowanceVO = (AllowanceDeductionVO) allowanceVOList.get(i);
-		
-			String deductionType = 	allowanceVO.getAllowancdeduction();
-		
+		for (int i = 0; i < allowanceVOList.size(); i++) {
+
+			AllowanceDeductionVO allowanceVO = (AllowanceDeductionVO) allowanceVOList.get(i);
+
+			String deductionType = allowanceVO.getAllowancdeduction();
+
 			if (deductionType.equals("allowance")) {
 				allowanceList.add(allowanceVO);
 			}
 			if (deductionType.equals("deduction")) {
-				deductionList.add(allowanceVO);				
+				deductionList.add(allowanceVO);
 			}
-			
+
 			System.out.println(deductionType);
 		}
-	
-		System.out.println("  DEDUCTION "+ deductionList.size());
+
+		System.out.println("  DEDUCTION " + deductionList.size());
 		System.out.println(" ALLOWANCE " + allowanceList.size());
-		
-		File f = new File(path+"//"+employeeVO2.getEmployeeId());
-		
+
+		File f = new File(path + "//" + employeeVO2.getEmployeeId());
+
 		f.mkdirs();
-		
-		String fileName = Basemethods.generatePaySlip(employeeVO2,attendanceVO,path+"//"+employeeVO2.getEmployeeId(),basicSalary,allowanceList,deductionList);
-		String link = Basemethods.uploadIntoS3(new File(fileName),employeeVO2.getEmployeeId());
+
+		String fileName = Basemethods.generatePaySlip(employeeVO2, attendanceVO,
+				path + "//" + employeeVO2.getEmployeeId(), basicSalary, allowanceList, deductionList);
+		String link = Basemethods.uploadIntoS3(new File(fileName), employeeVO2.getEmployeeId());
 		SalarySlipVO salarySlipVO = new SalarySlipVO();
-		
+
 		salarySlipVO.setSalarySlipLink(link);
 		salarySlipVO.setEmployeeVO(employeeVO2);
-		
-		
+
 		this.salarySlipService.insert(salarySlipVO);
 		return new ModelAndView("redirect:/admin/generatepay");
-		
+
 	}
-	
-	
 
 }
